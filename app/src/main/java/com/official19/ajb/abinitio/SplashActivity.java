8b087b7fb88b0d6106de394c6295647c5be435a4
Arvind Badwar <arvind.badwar19@gmail.com>
2018-07-23 08:44:15 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/official19/ajb/abinitio/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.official19.ajb.abinitio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+            }
+        }, 2000);
+    }
+}
