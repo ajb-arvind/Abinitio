d3fbfda79d7624be37b3e1e3a07ab68203d6d9ab
Dhanashri Jadhav <soni.jadhav2221@gmail.com>
2018-07-25 09:15:10 +0530

    Chnage in navigation drawer... Add navigation header background image

--- app/src/main/java/com/official19/ajb/abinitio/MainActivity.java
+++ app/src/main/java/com/official19/ajb/abinitio/MainActivity.java
@@ -2,9 +2,8 @@ package com.official19.ajb.abinitio;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,22 +81,36 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id){
+            case R.id.nav_home:
+                break;
 
-        } else if (id == R.id.nav_slideshow) {
+            case R.id.nav_auto:
+                break;
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.nav_civil:
+                break;
 
-        } else if (id == R.id.nav_share) {
+            case R.id.nav_entc:
+                break;
 
-        } else if (id == R.id.nav_send) {
+            case R.id.nav_instru:
+                break;
 
+            case R.id.nav_mech:
+                break;
+
+            case R.id.nav_share:
+                break;
+
+            case R.id.nav_send:
+                break;
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
